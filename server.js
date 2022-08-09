cbc2b029f44fbbcce8ddeb9835c0cbc9b1934fef
zubair <zubairfloat@gmail.com>
2022-08-10 00:30:07 +0500

    add base64 endpoint

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const http = require('http')
+const http = require("http");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
+  if (req.url === "/") return respondHello(req, res);
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res);
 
-  res.end()
-})
+  res.end();
+});
 
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+function respondHello(req, res) {
+  res.end(JSON.stringify({ msg: "hello" }));
 }
 
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, "");
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString("base64") }));
+}
 
-if (require.main !== module) module.exports = server
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
+
+if (require.main !== module) module.exports = server;
